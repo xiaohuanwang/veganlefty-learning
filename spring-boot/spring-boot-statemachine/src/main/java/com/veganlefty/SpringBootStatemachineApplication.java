8b8ad968e392565d77bd33b89e093726a69a28c6
王小欢 <wxh_work@163.com>
2022-07-06 15:56:26 +0800

    feat: spring statemachine 状态机

---
+++ spring-boot/spring-boot-statemachine/src/main/java/com/veganlefty/SpringBootStatemachineApplication.java
@@ -0,0 +1,13 @@
+package com.veganlefty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStatemachineApplication.class, args);
+	}
+
+}
---
+++ spring-boot/spring-boot-statemachine/src/test/java/com/veganlefty/SpringBootStatemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veganlefty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStatemachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
