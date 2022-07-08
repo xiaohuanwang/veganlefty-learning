b3b500d97a724e26f48ec49875aef11985d729e4
王小欢 <wxh_work@163.com>
2022-07-08 23:40:28 +0800

    init:初始化spring 工程

---
+++ spring/ioc-container-overview/src/main/java/com/example/ioccontaineroverview/IocContainerOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.ioccontaineroverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocContainerOverviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IocContainerOverviewApplication.class, args);
+    }
+
+}
---
+++ spring/ioc-container-overview/src/test/java/com/example/ioccontaineroverview/IocContainerOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ioccontaineroverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocContainerOverviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
