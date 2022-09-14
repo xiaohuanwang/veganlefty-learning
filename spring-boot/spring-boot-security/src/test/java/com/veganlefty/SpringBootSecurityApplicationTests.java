8c0970e6598e71927d64d91296a2dfe4be0a8d30
王小欢 <wxh_work@163.com>
2022-09-14 10:31:00 +0800

    build:调整配置文件

---
+++ spring-boot/spring-boot-security/src/test/java/com/veganlefty/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veganlefty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
