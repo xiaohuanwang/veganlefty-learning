f3a813dede402dfc2122eb62564ea4d6584feca1
王小欢 <wxh_work@163.com>
2022-12-28 10:06:49 +0800

    feat:加入GateWay网关demo

---
+++ spring-boot/spring-boot-gateway/src/main/java/com/veganlefty/SpringbootGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.veganlefty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootGatewayApplication.class, args);
+  }
+}
